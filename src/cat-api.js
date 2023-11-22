c4970930486ac03dbd5169a3d8e02ca7db425c16
Andrew Butenko <sedkol88@gmail.com>
2023-11-22 07:48:03 +0200

    222

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,41 +1,42 @@
-import axios from "axios";
-import { loader, error, breedSelect, catInfo } from "./index.js";
+import axios from 'axios';
+import { loader, error, breedSelect, catInfo } from './index.js';
 
 export function fetchBreeds() {
   loader.style.display = 'block';
   error.style.display = 'none';
   breedSelect.style.display = 'none';
-  
-  return axios.get('https://api.thecatapi.com/v1/breeds')
-  .then(response => {
-    const breeds = response.data;
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      breedSelect.appendChild(option);
-    });
-    
-    loader.style.display = 'none';
-    breedSelect.style.display = 'block';
-    
-    return breeds;
-  })
-    .catch(error => {
-    showError();
-  });
-}
 
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      const breeds = response.data;
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+
+      loader.style.display = 'none';
+      breedSelect.style.display = 'block';
+
+      return breeds;
+    })
+    .catch(error => {
+      showError();
+    });
+}
 
 export function fetchCatByBreed(breedId) {
   loader.style.display = 'block';
   error.style.display = 'none';
   catInfo.style.display = 'none';
-  
-  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-  .then(response => {
-    const cat = response.data[0];
-    const catInfoHTML = `
+
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      const cat = response.data[0];
+      const catInfoHTML = `
     <img src="${cat.url}" alt="Cat" width=300>
     <div>
     <p style="font-size: 28px;"><b>${cat.breeds[0].name}</b></p>
@@ -43,20 +44,18 @@ export function fetchCatByBreed(breedId) {
     <p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
     </div>
     `;
-    catInfo.innerHTML = catInfoHTML;
-    
-    loader.style.display = 'none';
-    catInfo.style.display = 'flex';
-    
-    return cat;
-  })
-  .catch(error => {
-    showError();
-  });
-  
+      catInfo.innerHTML = catInfoHTML;
+
+      loader.style.display = 'none';
+      catInfo.style.display = 'flex';
+
+      return cat;
+    })
+    .catch(error => {
+      showError();
+    });
 }
 
 function showError() {
-  loader.style.display = 'none';
-  // error.style.display = 'block';
+  error.style.display = 'block';
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const breedSelect = document.querySelector('.breed-select');
 export const loader = document.querySelector('.loader');
 export const error = document.querySelector('.error');
 export const catInfo = document.querySelector('.cat-info');
 
-import { fetchBreeds } from "./cat-api.js";
-import { fetchCatByBreed } from "./cat-api.js";
+import { fetchBreeds } from './cat-api.js';
+import { fetchCatByBreed } from './cat-api.js';
 
-axios.defaults.headers.common["x-api-key"] = "live_b0YdOabwcv6Luh4dhWKXNltRCdt6MTv6kmuB1jM5dlaQaJqs5ruTneNrzWEKdDrS";
+axios.defaults.headers.common['x-api-key'] =
+  'live_b0YdOabwcv6Luh4dhWKXNltRCdt6MTv6kmuB1jM5dlaQaJqs5ruTneNrzWEKdDrS';
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBreeds()
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBreeds();
 });
 
-breedSelect.addEventListener('change', function() {
+breedSelect.addEventListener('change', function () {
   const selectedBreedId = breedSelect.value;
-  
+
   if (selectedBreedId) {
     fetchCatByBreed(selectedBreedId);
   }
